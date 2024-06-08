065694a379b6cbbe116f9085ff9c560743ae1506
TLundComputing <33316738+TLundComputing@users.noreply.github.com>
2024-06-08 09:44:44 +0100

    Updates the page so that it is more responsive
    Works on using is-clipped on the html tag has also been added

--- CSConcepts/js/myScript.js
+++ CSConcepts/js/myScript.js
@@ -3,41 +3,52 @@ $(document).ready(function(){
     // USCH Triggers
     $("#uschcycle1Trigger").click(function(){
         $("#"+$("#uschcycle1Trigger").attr("data-target")).addClass("is-active");
+        $("html").addClass("is-clipped");
     });
     $("#uschcycle2Trigger").click(function(){
         $("#"+$("#uschcycle2Trigger").attr("data-target")).addClass("is-active");
+        $("html").addClass("is-clipped");
     });
     $("#uschcycle3Trigger").click(function(){
         $("#"+$("#uschcycle3Trigger").attr("data-target")).addClass("is-active");
+        $("html").addClass("is-clipped");
     });
 
     // MSCH Triggers
     $("#mschcycle1Trigger").click(function(){
         $("#"+$("#mschcycle1Trigger").attr("data-target")).addClass("is-active");
+        $("html").addClass("is-clipped");
     });
     $("#mschcycle2Trigger").click(function(){
         $("#"+$("#mschcycle2Trigger").attr("data-target")).addClass("is-active");
+        $("html").addClass("is-clipped");
     });
     $("#mschcycle3Trigger").click(function(){
         $("#"+$("#mschcycle3Trigger").attr("data-target")).addClass("is-active");
+        $("html").addClass("is-clipped");
     });
 
     // LSCH Triggers
     $("#lschcycle1Trigger").click(function(){
         $("#"+$("#lschcycle1Trigger").attr("data-target")).addClass("is-active");
+        $("html").addClass("is-clipped");
     });
     $("#lschcycle2Trigger").click(function(){
         $("#"+$("#lschcycle2Trigger").attr("data-target")).addClass("is-active");
+        $("html").addClass("is-clipped");
     });
     $("#lschcycle3Trigger").click(function(){
         $("#"+$("#lschcycle3Trigger").attr("data-target")).addClass("is-active");
+        $("html").addClass("is-clipped");
     });
 
     // Closing All Modals
     $(".modal-close").click(function(){
         $(".modal-close").parent().removeClass("is-active");
+        $("html").removeClass("is-clipped");
     });
     $(".modal-background").click(function(){
         $(".modal-background").parent().removeClass("is-active");
+        $("html").removeClass("is-clipped");
     });
 });
