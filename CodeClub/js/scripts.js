dd82920801c0ceee95375214fb9ef68d3774e884
TLundComputing <tomlund27@gmail.com>
2025-01-07 17:05:52 +0000

    Clean up and Addition of Code Club

---
+++ CodeClub/js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#stylePicker").change(function() {
+    let val = $("#stylePicker").children("option:selected").val();
+    if (val === "cd") {
+      $("link[href='./css/codeclub.css']").attr('href', './css/dojo.css');
+      $("#mainTitle").text("CoderDojo");
+    } else {
+      $("link[href='./css/dojo.css']").attr('href', './css/codeclub.css');
+      $("#mainTitle").text("Code Club @ Bootham");
+    }
+  });
+  $("#week1Btn").click(function() {
+    $("#week1Content").toggleClass("w3-show");
+    $("#week1Btn").toggleClass("accent");
+  });
+});
