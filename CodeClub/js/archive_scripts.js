eef63b518c680294c50a1ac26d034a9184f1453a
TLundComputing <33316738+TLundComputing@users.noreply.github.com>
2025-08-11 11:42:50 +0100

    Updated the archive for summer 25

--- CodeClub/js/archive_scripts.js
+++ CodeClub/js/archive_scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $("#stylePicker").change(function() {
+$(document).ready(function () {
+  $("#stylePicker").change(function () {
     let val = $("#stylePicker").children("option:selected").val();
     if (val === "cd") {
       $("link[href='./css/codeclub.css']").attr('href', './css/dojo.css');
@@ -9,50 +9,22 @@ $(document).ready(function() {
       $("#mainTitle").text("Code Club @ Bootham");
     }
   });
-  $("#week1Btn").click(function() {
-    $("#week1Content").toggleClass("w3-show");
-    $("#week1Btn").toggleClass("accent");
+  $(".modal-close").click(function () {
+    $(".w3-modal").hide();
   });
-  $("#week2Btn").click(function() {
-    $("#week2Content").toggleClass("w3-show");
-    $("#week2Btn").toggleClass("accent");
+  $("#spring25Btn").click(function () {
+    $("#spring25").show();
   });
-  $("#week3Btn").click(function() {
-    $("#week3Content").toggleClass("w3-show");
-    $("#week3Btn").toggleClass("accent");
+  $("#summer25Btn").click(function() {
+    $("#summer25").show();
   });
-  $("#week4Btn").click(function() {
-    $("#week4Content").toggleClass("w3-show");
-    $("#week4Btn").toggleClass("accent");
-  });
-  $("#week5Btn").click(function() {
-    $("#week5Content").toggleClass("w3-show");
-    $("#week5Btn").toggleClass("accent");
-  });
-  $("#week6Btn").click(function() {
-    $("#week6Content").toggleClass("w3-show");
-    $("#week6Btn").toggleClass("accent"); 
-  });
-  $("#week7Btn").click(function() {
-    $("#week7Content").toggleClass("w3-show");
-    $("#week7Btn").toggleClass("accent"); 
-  });
-  $("#week8Btn").click(function() {
-    $("#week8Content").toggleClass("w3-show");
-    $("#week8Btn").toggleClass("accent"); 
-  });
-  $("#week9Btn").click(function() {
-    $("#week9Content").toggleClass("w3-show");
-    $("#week9Btn").toggleClass("accent"); 
-  });
-  $("#week10Btn").click(function() {
-    $("#week10Content").toggleClass("w3-show");
-    $("#week10Btn").toggleClass("accent"); 
-  });
-  $(".modal-close").click(function(){
-	  $(".w3-modal").hide();
-  });
-  $("#spring25Btn").click(function(){
-	$("#spring25").show();
+  $("button").click(function(){
+    let id = $(this).attr('id');
+    console.log(id.localeCompare("summer25Btn"));
+    console.log(id.localeCompare("spring25Btn"));
+    if(id.localeCompare("summer25Btn") != 0 && id.localeCompare("spring25Btn") != 0) {
+      $("#" + id.slice(0,id.length - 3) + "Content").toggleClass("w3-show");
+      $("#" + id).toggleClass("accent");
+    }
   });
 });
