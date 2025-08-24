1749425ea79adaab8135fe3083f0fec5f7340c7a
TLundComputing <33316738+TLundComputing@users.noreply.github.com>
2025-08-24 12:57:18 +0100

    CC Autumn 25 created and hidden
    Set up the accordion for the next term. As the code club has not started yet, the outcome is hidden.

--- CodeClub/js/scripts.js
+++ CodeClub/js/scripts.js
@@ -9,44 +9,9 @@ $(document).ready(function() {
       $("#mainTitle").text("Code Club @ Bootham");
     }
   });
-  $("#week1Btn").click(function() {
-    $("#week1Content").toggleClass("w3-show");
-    $("#week1Btn").toggleClass("accent");
-  });
-  $("#week2Btn").click(function() {
-    $("#week2Content").toggleClass("w3-show");
-    $("#week2Btn").toggleClass("accent");
-  });
-  $("#week3Btn").click(function() {
-    $("#week3Content").toggleClass("w3-show");
-    $("#week3Btn").toggleClass("accent");
-  });
-  $("#week4Btn").click(function() {
-    $("#week4Content").toggleClass("w3-show");
-    $("#week4Btn").toggleClass("accent");
-  });
-  $("#week5Btn").click(function() {
-    $("#week5Content").toggleClass("w3-show");
-    $("#week5Btn").toggleClass("accent");
-  });
-  $("#week6Btn").click(function() {
-    $("#week6Content").toggleClass("w3-show");
-    $("#week6Btn").toggleClass("accent"); 
-  });
-  $("#week7Btn").click(function() {
-    $("#week7Content").toggleClass("w3-show");
-    $("#week7Btn").toggleClass("accent"); 
-  });
-  $("#week8Btn").click(function() {
-    $("#week8Content").toggleClass("w3-show");
-    $("#week8Btn").toggleClass("accent"); 
-  });
-  $("#week9Btn").click(function() {
-    $("#week9Content").toggleClass("w3-show");
-    $("#week9Btn").toggleClass("accent"); 
-  });
-  
-  $(".modal-close").click(function(){
-	  $(".w3-modal").hide();
+  $("button").click(function(){
+    let id = $(this).attr('id');
+    $("#" + id.slice(0,id.length - 3) + "Content").toggleClass("w3-show");
+    $("#" + id).toggleClass("accent");
   });
 });
